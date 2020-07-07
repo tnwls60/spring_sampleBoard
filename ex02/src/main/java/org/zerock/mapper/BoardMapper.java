c102aa8df232b99930347aa181ec4a4483370669
sujin <tnwls60@naver.com>
2020-07-07 17:45:52 +0900

    part3 비즈니스계층 CRUD/비즈니스 계층/프레젠테이션계층 CRUD 완료

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,10 +7,19 @@ import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("select * from tbl_board where bno > 0")
+	//xml파일에 처리함
+	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
 	
+	public void insert(BoardVO board);
 	
-	
+	public void insertSelectKey(BoardVO board);
 
+	public BoardVO read(Long bno);
+	
+	//int : 몇 건의 데이터가 삭제,수정되었는지 반환
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -16,10 +17,65 @@ public class BoardMapperTests {
 	
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
-	
+	/*
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("수진");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("수진");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testRead() {
+		
+		//존재하는 게시물 번호로 테스트  5번글 조회
+		BoardVO board = mapper.read(5l);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testDelete() {
+		//3번글 삭제 : 있으면 1, 없으면 0 반환
+		log.info("DELETE COUNT : " + mapper.delete(20L));
+	}
+	*/
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		//존재하는 번호인지 확인할 것
+		board.setBno(1l);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("작성자");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
 		
 	}
 }
