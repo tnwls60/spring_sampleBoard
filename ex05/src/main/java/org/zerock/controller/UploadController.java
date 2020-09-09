bd2a3a0454863ce2c7f3eaa4d9309a7fbae9cf0c
sujin <tnwls60@naver.com>
2020-09-09 20:40:10 +0900

    ch21 파일 업로드 방식 (form/ajax) 완성

---
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -0,0 +1,81 @@
+package org.zerock.controller;
+
+import java.io.File;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class UploadController {
+
+	@GetMapping("/uploadForm")
+	public void uploadForm() {
+		
+		log.info("upload form");
+	}
+	
+	@PostMapping("/uploadFormAction")
+	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+		
+		String uploadFolder = "C:\\upload";
+		
+		for (MultipartFile multipartFile : uploadFile) {
+			log.info("---------------------------------------");
+			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+			log.info("Upload File Size : " + multipartFile.getSize());
+			
+			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+			
+		}//end for
+	}
+	
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+		
+		log.info("upload ajax");
+	}
+	
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+		
+		log.info("update ajax post..............");
+		
+		String uploadFolder = "C:\\upload";
+		
+		for (MultipartFile multipartFile : uploadFile) {
+			
+			log.info("--------------------------");
+			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+			log.info("Upload File Size : " + multipartFile.getSize());
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			//IE는 path로 나오기 떄문에 substring()으로 잘라옴
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			
+			log.info("only file name: " + uploadFileName);
+			
+			File saveFile = new File(uploadFolder, uploadFileName);
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+		}//for 끝
+		
+	}
+	
+}
