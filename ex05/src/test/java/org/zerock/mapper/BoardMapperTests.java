8e76e1dc373f60be71a4d4f66891120ee5f342dd
sujin <tnwls60@naver.com>
2020-10-20 17:12:17 +0900

    ch25를 위해 이전 설정 가져옴

---
+++ ex05/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,112 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	/*
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("수진");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("수진");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testRead() {
+		
+		//존재하는 게시물 번호로 테스트  5번글 조회
+		BoardVO board = mapper.read(5l);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testDelete() {
+		//3번글 삭제 : 있으면 1, 없으면 0 반환
+		log.info("DELETE COUNT : " + mapper.delete(20L));
+	}
+	
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		//존재하는 번호인지 확인할 것
+		board.setBno(1l);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("작성자");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
+	}
+	
+	
+	@Test
+	public void testPaging() {
+		
+		Criteria cri = new Criteria();
+		//10개씩 3페이지
+		cri.setPageNum(3);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board.getBno()));
+	}
+	*/
+	
+	@Test
+	public void testSearch() {
+		
+		Criteria cri = new Criteria();
+		cri.setKeyword("새로");
+		cri.setType("TCW");
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board));
+		
+	}
+	
+	
+}
