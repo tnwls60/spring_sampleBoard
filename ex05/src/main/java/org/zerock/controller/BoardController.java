78913791ec3f4dbfb29243987d300b9ef503de0d
sujin <tnwls60@naver.com>
2020-10-22 17:04:31 +0900

    ch25 진행중

--- ex05/src/main/java/org/zerock/controller/BoardController.java
+++ ex05/src/main/java/org/zerock/controller/BoardController.java
@@ -49,11 +49,21 @@ public class BoardController {
 	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		
+		log.info("===============================");
+		
 		log.info("register: " + board);
 		
-		service.register(board);
+		if(board.getAttachList() != null) {
+			
+			board.getAttachList().forEach(attach -> log.info(attach));
+			
+		}
 		
-		rttr.addFlashAttribute("result", board.getBno());
+		log.info("===============================");
+		
+		//service.register(board);
+		
+		//rttr.addFlashAttribute("result", board.getBno());
 		
 		return "redirect:/board/list";
 	}
