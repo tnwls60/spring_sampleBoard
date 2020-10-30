5b005d66c50336484370165a285549c5c5cbd531
sujin <tnwls60@naver.com>
2020-10-30 19:22:20 +0900

    ch28 완료

--- ex05/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex05/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -81,10 +81,26 @@ public class BoardServiceImpl implements BoardService {
 		return mapper.read(bno);
 	}
 	
+	@Transactional
 	@Override
 	public boolean modify(BoardVO board) {
 		log.info("modify......." + board);
-		return mapper.update(board) == 1;
+		
+		attachMapper.deleteAll(board.getBno());
+		
+		boolean modifyResult = mapper.update(board) == 1;
+		
+		//수정되고 첨부파일이 있으면
+		if(modifyResult && board.getAttachList() != null && board.getAttachList().size() > 0) {
+			
+			board.getAttachList().forEach(attach -> {
+				
+				attach.setBno(board.getBno());
+				attachMapper.insert(attach);
+			});
+		}
+		
+		return modifyResult;
 	}
 	
 	/*@Override
